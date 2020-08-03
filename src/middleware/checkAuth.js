780edde60905edb41dc08db66f2d02c7a85f8101
Kacper Kaźmierkiewicz <38405516+KaZiUpl@users.noreply.github.com>
2020-08-03 14:21:31 +0200

    Update checkAuth.js

--- src/middleware/checkAuth.js
+++ src/middleware/checkAuth.js
@@ -5,13 +5,10 @@ const User = require('../models/user.model');
 
 module.exports = async function (req, res, next) {
   try {
-      console.log(Math.max());
-      console.log(Math.min());
     // get access token from authorization header
     const token = req.headers.authorization.split(' ')[1];
     const decoded = jwt.verify(token, dotenv.jwtSecret);
     req.userData = decoded;
-    console.log(decoded);
 
     next();
   } catch (error) {
