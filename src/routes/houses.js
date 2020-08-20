cc32f1c5106f5a7b4e7ca0525ce39fb7f137fbbf
Kacper Kaźmierkiewicz <38405516+KaZiUpl@users.noreply.github.com>
2020-08-20 23:22:02 +0200

    added basic apidoc setup

--- src/routes/houses.js
+++ src/routes/houses.js
@@ -6,14 +6,49 @@ const housesController = require('../controllers/houses');
 
 router.use(checkAuthMiddleware);
 
+/**
+ * @apiDefine House House
+ * Endpoints for managing information about user's main containers.
+ */
+
+/**
+ * @api {post} /houses Create new main container (house)
+ * @apiName PostHouse
+ * @apiGroup House
+ * @apiPermission logged in user
+ */
 router.post('/', housesController.createHouse);
 
+/**
+ * @api {get} /houses Get list of user's main containers (houses).
+ * @apiName GetHouseList
+ * @apiGroup House
+ * @apiPermission logged in user
+ */
 router.get('/', housesController.getHouseList);
 
+/**
+ * @api {get} /houses/:id Get main container (house) info.
+ * @apiName GetHouseInfo
+ * @apiGroup House
+ * @apiPermission main container's owner or collaborators
+ */
 router.get('/:id', housesController.getHouse);
 
+/**
+ * @api {put} /houses/:id Modify main container (house) info.
+ * @apiName PutHouseInfo
+ * @apiGroup House
+ * @apiPermission main container's owner
+ */
 router.put('/:id', housesController.editHouse);
 
+/**
+ * @api {delete} /houses/:id Delete main container (house) with it's content.
+ * @apiName DeleteHouse
+ * @apiGroup House
+ * @apiPermission main container's owner
+ */
 router.delete('/:id', housesController.deleteHouse);
 
 module.exports = router;
