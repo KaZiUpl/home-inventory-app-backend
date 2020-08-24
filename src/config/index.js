1777c539aafe1a016f7c7eaeb5c278df6954ce31
Kacper Kaźmierkiewicz <38405516+KaZiUpl@users.noreply.github.com>
2020-08-24 18:10:53 +0200

    code formatting

--- src/config/index.js
+++ src/config/index.js
@@ -2,8 +2,8 @@ const dotenv = require('dotenv');
 
 dotenv.config();
 
-module.exports =  {
-    port: process.env.PORT,
-    mongodbUri: process.env.MONGODB_URI,
-    jwtSecret: process.env.JWT_SECRET
-}
+module.exports = {
+  port: process.env.PORT,
+  mongodbUri: process.env.MONGODB_URI,
+  jwtSecret: process.env.JWT_SECRET
+};
