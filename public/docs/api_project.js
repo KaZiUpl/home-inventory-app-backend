d1a7fcec8f06819af049d22a016a641573282e26
Kacper Kaźmierkiewicz <38405516+KaZiUpl@users.noreply.github.com>
2020-08-28 21:07:46 +0200

    updated documentation

--- public/docs/api_project.js
+++ public/docs/api_project.js
@@ -9,7 +9,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2020-08-24T16:06:59.274Z",
+    "time": "2020-08-28T19:00:45.398Z",
     "url": "http://apidocjs.com",
     "version": "0.24.0"
   }
--- src/routes/houses.js
+++ src/routes/houses.js
@@ -8,16 +8,16 @@ router.use(checkAuthMiddleware);
 
 /**
  * @apiDefine House House
- * Endpoints for managing information about user's main containers.
+ * Endpoints for managing information about user's houses called houses. Every house has one owner that can change house info and add other users as collaborators.
  */
 
 /**
- * @api {post} /houses Create new main container (house)
+ * @api {post} /houses Create new house
  * @apiName PostHouse
  * @apiGroup House
  * @apiPermission logged in user
- * @apiParam {String} name Name of the main container (house)
- * @apiParam {String} description Description of the main container (house)
+ * @apiParam {String} name Name of the house
+ * @apiParam {String} description Description of the house
  * @apiSuccess (Success 200) {String} message Response message
  * @apiSuccessExample {json} Response(example):
  * {
@@ -27,12 +27,12 @@ router.use(checkAuthMiddleware);
 router.post('/', housesController.createHouse);
 
 /**
- * @api {post} /houses/:id/collaborators Add collaborator to a main container (house)
+ * @api {post} /houses/:id/collaborators Add a collaborator
  * @apiName PostAddCollaborator
  * @apiGroup House
- * @apiPermission main container's owner
- * @apiDescription Adds a user as a collaborator. First user with login of email matching the provided name is added.
- * @apiParam {String} id main container's id
+ * @apiPermission house owner
+ * @apiDescription Adds a user as a collaborator. First user with matching login or email is added.
+ * @apiParam {String} id house id
  * @apiParam {String} name login or email of the collaborator
  * @apiSuccess (Success 200) {String} message Response message
  * @apiSuccessExample {json} Response(example):
@@ -46,8 +46,8 @@ router.post('/:id/collaborators', housesController.addCollaborator);
  * @api {get} /houses/:id/collaborators Get the list of collaborators
  * @apiName GetCollaboratorList
  * @apiGroup House
- * @apiPermission main container's owner or collaborator
- * @apiParam {String} id main container's id
+ * @apiPermission House owner or collaborator
+ * @apiParam {String} id house's id
  * @apiSuccess (Success 200) {Object[]} users Array of collaborators
  * @apiSuccess (Success 200) {String} _id Collaborator's id
  * @apiSuccess (Success 200) {String} login Collaborator's login
@@ -62,16 +62,16 @@ router.post('/:id/collaborators', housesController.addCollaborator);
 router.get('/:id/collaborators', housesController.getCollaborators);
 
 /**
- * @api {get} /houses Get list of user's main containers (houses).
+ * @api {get} /houses Get list of houses
  * @apiName GetHouseList
  * @apiGroup House
  * @apiPermission logged in user
- * @apiDescription Returns a list of houses that user owns of is a collaborator in
- * @apiSuccess (Success 200) {Object[]} houses Array of user's main containers
+ * @apiDescription Returns a list of houses that user owns or is collaborating in.
+ * @apiSuccess (Success 200) {Object[]} houses Array of user's houses
  * @apiSuccess (Success 200) {String[]} collaborators Array of collaborator's id's
- * @apiSuccess (Success 200) {String} _id Main container's id
- * @apiSuccess (Success 200) {String} name Name of the main container
- * @apiSuccess (Success 200) {String} description Description of the main container
+ * @apiSuccess (Success 200) {String} _id House id
+ * @apiSuccess (Success 200) {String} name Name of the house
+ * @apiSuccess (Success 200) {String} description Description of the house
  * @apiSuccess (Success 200) {Object} owner Object with owner's id and login
  * @apiSuccessExample {json} Success-Response:
  *[
@@ -93,16 +93,16 @@ router.get('/:id/collaborators', housesController.getCollaborators);
 router.get('/', housesController.getHouseList);
 
 /**
- * @api {get} /houses/:id Get main container (house) info.
+ * @api {get} /houses/:id Get house info
  * @apiName GetHouseInfo
  * @apiGroup House
- * @apiPermission main container's owner or collaborators
- * @apiParam {String} id ID of the main container (house)
- * @apiSuccess (Success 200) {Object} Requested main container
+ * @apiPermission house owner or collaborators
+ * @apiParam {String} id ID of the house
+ * @apiSuccess (Success 200) {Object} Requested house
  * @apiSuccess (Success 200) {Object[]} collaborators Array of collaborators' logins and ids
- * @apiSuccess (Success 200) {String} _id Main container's id
- * @apiSuccess (Success 200) {String} name Name of the main container
- * @apiSuccess (Success 200) {String} description Description of the main container
+ * @apiSuccess (Success 200) {String} _id house's id
+ * @apiSuccess (Success 200) {String} name Name of the house
+ * @apiSuccess (Success 200) {String} description Description of the house
  * @apiSuccess (Success 200) {Object} owner Object with owner's id and login
  * @apiSuccessExample {json} Success-Response:
  *{
@@ -125,12 +125,12 @@ router.get('/', housesController.getHouseList);
 router.get('/:id', housesController.getHouse);
 
 /**
- * @api {put} /houses/:id Modify main container (house) info.
+ * @api {put} /houses/:id Modify house info
  * @apiName PutHouseInfo
  * @apiGroup House
- * @apiPermission main container's owner
- * @apiParam {String} name New name of the main container (house)
- * @apiParam {String} description New description of the main container (house)
+ * @apiPermission house owner
+ * @apiParam {String} name New name of the house
+ * @apiParam {String} description New description of the house
  * @apiSuccess (Success 200) {String} message Response message
  * @apiSuccessExample Success-Response:
  *     {
@@ -140,11 +140,11 @@ router.get('/:id', housesController.getHouse);
 router.put('/:id', housesController.editHouse);
 
 /**
- * @api {delete} /houses/:id Delete main container (house) with it's content.
+ * @api {delete} /houses/:id Delete a house
  * @apiName DeleteHouse
  * @apiGroup House
- * @apiPermission main container's owner
- * @apiParam {String} id Main container's id
+ * @apiPermission house owner
+ * @apiParam {String} id house id
  * @apiSuccess {String} message Response message
  * @apiSuccessExample {json} Success-Response:
  *     {
@@ -154,12 +154,12 @@ router.put('/:id', housesController.editHouse);
 router.delete('/:id', housesController.deleteHouse);
 
 /**
- * @api {delete} /houses/:id/collaborators Delete a collaborator from a main container (house)
+ * @api {delete} /houses/:id/collaborators Remove a collaborator
  * @apiName DeleteCollaborator
  * @apiGroup House
- * @apiPermission main container's owner or collaborator
- * @apiDescription Deletes user with provided id from collaborators list. Can be used either by main container's owner or one of its collaborators in order to remove oneself from collaborating.
- * @apiParam {String} id main container's id
+ * @apiPermission house owner or collaborator
+ * @apiDescription Removes user with provided id from collaborators list. Can be used either by house owner or one of its collaborators in order to remove oneself from collaborating.
+ * @apiParam {String} id house id
  * @apiParam {String} id collaborator's id
  * @apiSuccess (Success 200) {String} message Response message
  * @apiSuccessExample {json} Response(example):
