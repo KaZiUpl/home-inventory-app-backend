2e42016e277aaff4fa8d0cf40222053c53ed6b08
Kacper Kaźmierkiewicz <38405516+KaZiUpl@users.noreply.github.com>
2020-09-09 12:28:26 +0200

    updated auth middleware and added new dependencies

--- src/middleware/checkAuth.js
+++ src/middleware/checkAuth.js
@@ -2,18 +2,21 @@ const jwt = require('jsonwebtoken');
 const dotenv = require('../config/index');
 
 const User = require('../models/user.model');
+const { UnauthorizedError } = require('../error/errors');
 
 module.exports = async function (req, res, next) {
   try {
     // get access token from authorization header
-    const token = req.headers.authorization.split(' ')[1];
-    const decoded = jwt.verify(token, dotenv.jwtSecret);
+    const authHeader = req.get('Authorization');
+    if (!authHeader) {
+      throw new UnauthorizedError();
+    }
+    const token = authHeader.split(' ')[1];
+    const decoded = await jwt.verify(token, dotenv.jwtSecret);
     req.userData = decoded;
 
     next();
   } catch (error) {
-    return res
-      .status(401)
-      .json({ message: 'You have to log in to access this resource.' });
+    next(error);
   }
 };
