1e5f6b846e82b72325807295aca13b9953f14b78
Kacper Kaźmierkiewicz <38405516+KaZiUpl@users.noreply.github.com>
2020-09-09 13:02:52 +0200

    added tests for checkAuthMiddleware

--- src/middleware/checkAuth.js
+++ src/middleware/checkAuth.js
@@ -4,7 +4,7 @@ const dotenv = require('../config/index');
 const User = require('../models/user.model');
 const { UnauthorizedError } = require('../error/errors');
 
-module.exports = async function (req, res, next) {
+module.exports = function (req, res, next) {
   try {
     // get access token from authorization header
     const authHeader = req.get('Authorization');
@@ -12,11 +12,14 @@ module.exports = async function (req, res, next) {
       throw new UnauthorizedError();
     }
     const token = authHeader.split(' ')[1];
-    const decoded = await jwt.verify(token, dotenv.jwtSecret);
+    if (!token) {
+      throw new UnauthorizedError();
+    }
+    const decoded = jwt.verify(token, dotenv.jwtSecret);
     req.userData = decoded;
 
     next();
   } catch (error) {
-    next(error);
+    throw error;
   }
 };
---
+++ src/test/checkAuth.test.js
@@ -0,0 +1,49 @@
+const expect = require('chai').expect;
+const sinon = require('sinon');
+const jwt = require('jsonwebtoken');
+
+const checkAuthMiddleware = require('../middleware/checkAuth');
+const { UnauthorizedError } = require('../error/errors');
+
+describe('CheckAuthMiddleware', function () {
+  it('should throw UnauthorizedError if no Authorization header is present', function () {
+    const req = {
+      get: function (headerName) {
+        return null;
+      }
+    };
+
+    expect(checkAuthMiddleware.bind(this, req, {}, () => {})).to.throw(
+      UnauthorizedError
+    );
+  });
+
+  it('should throw UnauthorizedError if Authorization header is malformed', function () {
+    const req = {
+      get: function (headerName) {
+        return 'xyz';
+      }
+    };
+
+    expect(checkAuthMiddleware.bind(this, req, {}, () => {})).to.throw(
+      UnauthorizedError
+    );
+  });
+
+  it('should add user data to userData property after decoding the token', function () {
+    const req = {
+      get: function (headerName) {
+        return 'Bearer xyz';
+      }
+    };
+
+    sinon.stub(jwt, 'verify');
+    jwt.verify.returns({ id: 'abc' });
+    checkAuthMiddleware(req, {}, () => {});
+
+    expect(jwt.verify.called).to.be.true;
+    expect(req).to.have.property('userData');
+    expect(req.userData).to.have.property('id', 'abc');
+    jwt.verify.restore();
+  });
+});
