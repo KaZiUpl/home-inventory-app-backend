69db2f8538fef7e55ea4625308622da69cfea3df
Kacper Kaźmierkiewicz <38405516+KaZiUpl@users.noreply.github.com>
2020-10-22 23:48:43 +0200

    added checking whether the user is logged in

--- src/middleware/checkAuth.js
+++ src/middleware/checkAuth.js
@@ -4,7 +4,7 @@ const dotenv = require('../config/index');
 const User = require('../models/user.model');
 const { UnauthorizedError } = require('../error/errors');
 
-module.exports = function (req, res, next) {
+module.exports = async function (req, res, next) {
   try {
     // get access token from authorization header
     const authHeader = req.get('Authorization');
@@ -15,11 +15,21 @@ module.exports = function (req, res, next) {
     if (!token) {
       throw new UnauthorizedError();
     }
-    const decoded = jwt.verify(token, dotenv.jwtSecret);
+    let decoded;
+    try {
+      decoded = jwt.verify(token, dotenv.jwtSecret);
+    } catch (error) {
+      throw new UnauthorizedError();
+    }
     req.userData = decoded;
+
+    let user = await User.findById(req.userData.id);
+    if (!user.refresh_token) {
+      throw new UnauthorizedError();
+    }
 
     next();
   } catch (error) {
-    throw error;
+    next(error);
   }
 };
