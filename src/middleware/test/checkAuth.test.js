0ee0b95f70cbfcd793a3d0cc1eb5de795f25cd63
Kacper Kaźmierkiewicz <38405516+KaZiUpl@users.noreply.github.com>
2020-10-24 20:44:11 +0200

    Update checkAuth.test.js

--- src/middleware/test/checkAuth.test.js
+++ src/middleware/test/checkAuth.test.js
@@ -1,35 +1,90 @@
-const expect = require('chai').expect;
+const chai = require('chai');
 const sinon = require('sinon');
 const jwt = require('jsonwebtoken');
+const mongoose = require('mongoose');
+const chaiAsPromised = require('chai-as-promised');
+const expect = chai.expect;
+
+chai.use(chaiAsPromised);
 
 const checkAuthMiddleware = require('../checkAuth');
 const { UnauthorizedError } = require('../../error/errors');
+const User = require('../../models/user.model');
 
 describe('CheckAuthMiddleware', function () {
-  it('should throw UnauthorizedError if no Authorization header is present', function () {
+  it('should throw UnauthorizedError if no Authorization header is present', async function () {
     const req = {
       get: function (headerName) {
         return null;
       }
     };
+    function next(error) {
+      throw error;
+    }
 
-    expect(checkAuthMiddleware.bind(this, req, {}, () => {})).to.throw(
+    await expect(checkAuthMiddleware(req, {}, next)).to.be.rejectedWith(
       UnauthorizedError
     );
   });
-
-  it('should throw UnauthorizedError if Authorization header is malformed', function () {
+  it('should throw UnauthorizedError if Authorization header is malformed', async function () {
     const req = {
       get: function (headerName) {
         return 'xyz';
       }
     };
+    function next(error) {
+      throw error;
+    }
 
-    expect(checkAuthMiddleware.bind(this, req, {}, () => {})).to.throw(
+    await expect(checkAuthMiddleware(req, {}, next)).to.be.rejectedWith(
       UnauthorizedError
     );
   });
+  it('should throw UnauthorizedError if user is not logged in', async function () {
+    //create db connection
+    await mongoose.connect(process.env.MONGO_TEST_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true
+    });
+    // create user
+    let user = await User.create({
+      login: 'kacperkaz',
+      email: 'kacperkaz@example.com',
+      password: 'asd',
+      role: 'user'
+    });
+    // create access token
+    const tokenTimestamp = Math.floor(Date.now() / 1000) + 15 * 60; // expires in 15 minutes
+    const tokenExpDate = new Date(tokenTimestamp * 1000);
 
+    const token = jwt.sign(
+      {
+        login: user.login,
+        email: user.email,
+        id: user._id,
+        role: user.role,
+        exp: tokenTimestamp
+      },
+      process.env.JWT_SECRET
+    );
+    const req = {
+      get: function (headerName) {
+        return token;
+      }
+    };
+    function next(error) {
+      throw error;
+    }
+
+    await expect(checkAuthMiddleware(req, {}, next)).to.be.rejectedWith(
+      UnauthorizedError
+    );
+
+    //clean db
+    await User.deleteMany({});
+    await mongoose.connection.close();
+  });
   it('should add user data to userData property after decoding the token', function () {
     const req = {
       get: function (headerName) {
