28f62e724cbb8fcea8c0aae222e32bb55039d2eb
Kacper Kaźmierkiewicz <38405516+KaZiUpl@users.noreply.github.com>
2020-10-28 23:05:55 +0100

    Update users.service.test.js

--- src/services/test/users.service.test.js
+++ src/services/test/users.service.test.js
@@ -14,10 +14,10 @@ const {
   ForbiddenError
 } = require('../../error/errors');
 
-const UsersController = require('../users');
+const UsersService = require('../users.service');
 const User = require('../../models/user.model');
 
-describe('Users Controller', function () {
+describe('Users Service', function () {
   before(async function () {
     // connect to test db
     await mongoose.connect(process.env.MONGO_TEST_URI, {
@@ -40,102 +40,59 @@ describe('Users Controller', function () {
       await User.deleteMany({});
     });
 
-    it('should throw BadRequestError if user with login already exist', async function () {
-      //create test user
-      await User.create({
-        login: 'kacperkaz',
-        email: 'kacperkaz@example.com',
-        password: 'asd',
-        role: 'admin'
-      });
-
-      const req = {
-        body: {
-          login: 'kacperkaz',
-          email: 'kacper@example.com',
-          password: 'asd'
-        }
-      };
-
-      await expect(UsersController.createNewUser(req)).to.be.rejectedWith(
-        BadRequestError
-      );
-    });
-
-    it('should throw BadRequestError if user with email already exist', async function () {
-      //create test user
-      await User.create({
-        login: 'kacperkaz',
-        email: 'kacperkaz@example.com',
-        password: 'asd',
-        role: 'admin'
-      });
-
-      const req = {
-        body: {
-          login: 'kacperaz',
-          email: 'kacperkaz@example.com',
-          password: 'asd'
-        }
-      };
-      await expect(UsersController.createNewUser(req)).to.be.rejectedWith(
-        BadRequestError
-      );
-    });
-
     it('should create a new user', async function () {
-      //create test user
-      const req = {
-        body: {
-          login: 'kacperkaz',
-          email: 'kacperkaz@example.com',
-          password: 'asd'
-        }
+      const body = {
+        login: 'kacperkaz',
+        email: 'kacperkaz@example.com',
+        password: 'asd'
       };
-      let response = await UsersController.createNewUser(req);
+
+      let response = await UsersService.createUser(
+        body.login,
+        body.email,
+        body.password
+      );
       let users = await User.find({});
 
-      expect(response).to.have.property('message');
+      expect(response).to.be.instanceOf(mongoose.Types.ObjectId);
       expect(users.length).to.equal(1);
     });
+    it('should throw if user with login already exist', async function () {
+      //create test user
+      await User.create({
+        login: 'kacperkaz',
+        email: 'kacperkaz@example.com',
+        password: 'asd',
+        role: 'admin'
+      });
 
-    it('should throw UnprocessableEntityError if login is not present in body', async function () {
-      const req = {
-        body: {
-          email: 'kacperkaz@example.com',
-          password: 'asd'
-        }
+      const body = {
+        login: 'kacperkaz',
+        email: 'kacper@example.com',
+        password: 'asd'
       };
 
-      await expect(UsersController.createNewUser(req)).to.be.rejectedWith(
-        UnprocessableEntityError
-      );
+      await expect(
+        UsersService.createUser(body.login, body.email, body.password)
+      ).to.be.rejected;
     });
+    it('should throw if user with email already exist', async function () {
+      //create test user
+      await User.create({
+        login: 'kacperkaz',
+        email: 'kacperkaz@example.com',
+        password: 'asd',
+        role: 'admin'
+      });
 
-    it('should throw UnprocessableEntityError if email is not present in body', async function () {
-      const req = {
-        body: {
-          login: 'kacperkaz',
-          password: 'asd'
-        }
+      const body = {
+        login: 'kacperaz',
+        email: 'kacperkaz@example.com',
+        password: 'asd'
       };
-
-      await expect(UsersController.createNewUser(req)).to.be.rejectedWith(
-        UnprocessableEntityError
-      );
-    });
-
-    it('should throw UnprocessableEntityError if password is not present in body', async function () {
-      const req = {
-        body: {
-          login: 'kacperkaz',
-          email: 'kacperkaz@example.com'
-        }
-      };
-
-      await expect(UsersController.createNewUser(req)).to.be.rejectedWith(
-        UnprocessableEntityError
-      );
+      await expect(
+        UsersService.createUser(body.login, body.email, body.password)
+      ).to.be.rejected;
     });
   });
   describe('Change login', function () {
@@ -157,28 +114,17 @@ describe('Users Controller', function () {
         role: 'user'
       });
 
-      const req = {
-        body: {
-          login: 'kacperaz'
-        },
-        userData: {
-          id: user.id,
-          email: 'kacperkaz@example.com'
-        },
-        params: {
-          id: user.id
-        }
-      };
+      const newLogin = 'kacperaz';
+
+      await expect(UsersService.changeLogin(user._id, newLogin)).to.be
+        .fulfilled;
 
       //get user after modifying login
-      let response = await UsersController.changeLogin(req);
       let newUser = await User.findById(user._id);
 
-      expect(response).to.have.property('message');
-      expect(newUser).to.have.property('login', 'kacperaz');
+      expect(newUser).to.have.property('login', newLogin);
     });
-
-    it('should throw BadRequestError if provided login is taken', async function () {
+    it('should throw if provided login is taken', async function () {
       //create first user
       let user = await User.create({
         login: 'kacperkaz',
@@ -194,56 +140,10 @@ describe('Users Controller', function () {
         role: 'user'
       });
 
-      const req = {
-        body: {
-          login: 'kacperkaz'
-        },
-        userData: {
-          id: user2.id,
-          email: 'asd@example.com'
-        },
-        params: {
-          id: user2.id
-        }
-      };
+      const newLogin = 'kacperkaz';
 
-      await expect(UsersController.changeLogin(req)).to.be.rejectedWith(
-        BadRequestError
-      );
-    });
-
-    it('should throw ForbiddenError when trying to modify someone else login', async function () {
-      //create first user
-      let user = await User.create({
-        login: 'kacperkaz',
-        email: 'kacperkaz@example.com',
-        password: 'asd',
-        role: 'user'
-      });
-      //create second user
-      let user2 = await User.create({
-        login: 'asd',
-        email: 'asd@example.com',
-        password: 'asd',
-        role: 'user'
-      });
-
-      const req = {
-        body: {
-          login: 'sdf'
-        },
-        userData: {
-          id: user.id,
-          email: 'kacperkaz@example.com'
-        },
-        params: {
-          id: user2.id
-        }
-      };
-
-      await expect(UsersController.changeLogin(req)).to.be.rejectedWith(
-        ForbiddenError
-      );
+      await expect(UsersService.changeLogin(user2._id, newLogin)).to.be
+        .rejected;
     });
   });
   describe('Get user', function () {
@@ -265,55 +165,18 @@ describe('Users Controller', function () {
         role: 'user'
       });
 
-      const req = {
-        params: {
-          id: user._id
-        },
-        userData: {
-          id: user._id
-        }
-      };
-      let response = await (await UsersController.getUser(req)).toJSON();
+      let response = await (await UsersService.getUser(user._id)).toJSON();
 
       expect(response).to.be.a('object');
       expect(response).to.not.have.property('_id');
       expect(response).to.have.property('login');
       expect(response).to.have.property('email');
     });
-
-    it('should throw ForbiddenError when trying to access other user data', async function () {
-      //create user
-      let user = await User.create({
-        login: 'kacperkaz',
-        email: 'kacperkaz@example.com',
-        password: 'asd',
-        role: 'user'
-      });
-      //create user 2
-      let user2 = await User.create({
-        login: 'asd',
-        email: 'asd@example.com',
-        password: 'asd',
-        role: 'user'
-      });
-    });
-
-    it('should throw BadRequestError when trying to access non-existent user data', async function () {
+    it('should throw when trying to access non-existent user data', async function () {
       //create id
-      let id = mongoose.Types.ObjectId();
+      const id = mongoose.Types.ObjectId();
 
-      const req = {
-        params: {
-          id: id
-        },
-        userData: {
-          id: id
-        }
-      };
-
-      await expect(UsersController.getUser(req)).to.be.rejectedWith(
-        BadRequestError
-      );
+      await expect(UsersService.getUser(id)).to.be.rejected;
     });
   });
   describe('Put user', function () {
@@ -334,55 +197,16 @@ describe('Users Controller', function () {
         role: 'user'
       });
 
-      const req = {
-        body: {
-          login: 'asd'
-        },
-        params: {
-          id: user._id
-        },
-        userData: {
-          id: user._id
-        }
+      const body = {
+        login: 'asd'
       };
 
-      let response = await UsersController.modifyUser(req);
+      await expect(UsersService.modifyUser(user._id, body)).to.be.fulfilled;
+
       let updatedUser = await User.findById(user._id);
 
-      expect(updatedUser).to.have.property('login', 'asd');
+      expect(updatedUser).to.have.property('login', body.login);
     });
-
-    it("should throw ForbiddenError when trying to change other user's data", async function () {
-      let user1 = await User.create({
-        login: 'kacperkaz',
-        email: 'kacperkaz@example.com',
-        password: 'asd',
-        role: 'user'
-      });
-      let user2 = await User.create({
-        login: 'asd',
-        email: 'asd@example.com',
-        password: 'asd',
-        role: 'user'
-      });
-
-      const req = {
-        body: {
-          login: 'asd'
-        },
-        params: {
-          id: user2._id
-        },
-        userData: {
-          id: user1._id
-        }
-      };
-
-      await expect(UsersController.modifyUser(req)).to.be.rejectedWith(
-        ForbiddenError
-      );
-    });
-
     it("should not update user's role, email or password", async function () {
       let user = await User.create({
         login: 'kacperkaz',
@@ -391,21 +215,14 @@ describe('Users Controller', function () {
         role: 'user'
       });
 
-      const req = {
-        body: {
-          email: 'asd@example.com',
-          password: 'sdf',
-          role: 'admin'
-        },
-        params: {
-          id: user._id
-        },
-        userData: {
-          id: user._id
-        }
+      const body = {
+        email: 'asd@example.com',
+        password: 'sdf',
+        role: 'admin'
       };
 
-      let response = await UsersController.modifyUser(req);
+      await expect(UsersService.modifyUser(user._id, body)).to.be.fulfilled;
+
       let updatedUser = await User.findById(user._id);
 
       expect(updatedUser).to.have.property('email', 'kacperkaz@example.com');
@@ -433,20 +250,17 @@ describe('Users Controller', function () {
         role: 'user'
       });
 
-      const req = {
-        body: {
-          login: 'kacperkaz',
-          password: 'asd'
-        }
+      const body = {
+        login: 'kacperkaz',
+        password: 'asd'
       };
 
-      let response = await UsersController.login(req);
+      let response = await UsersService.login(body.login, body.password);
 
       expect(response).to.be.a('object');
       expect(response).to.have.property('access_token');
       expect(response).to.have.property('refresh_token');
     });
-
     it('should add refresh token to the user in a database', async function () {
       //create user
       let user = await User.create({
@@ -457,22 +271,20 @@ describe('Users Controller', function () {
         role: 'user'
       });
 
-      const req = {
-        body: {
-          login: 'kacperkaz',
-          password: 'asd'
-        }
+      const body = {
+        login: 'kacperkaz',
+        password: 'asd'
       };
 
-      await UsersController.login(req);
+      await expect(UsersService.login(body.login, body.password)).to.be
+        .fulfilled;
 
       let dbUser = await User.findById(user._id);
 
       expect(dbUser).to.have.property('refresh_token');
       expect(dbUser.refresh_token).to.be.a('string');
     });
-
-    it('should throw BadRequestError when trying to log in with invalid credentials', async function () {
+    it('should throw when trying to log in with invalid credentials', async function () {
       //create user
       let user = await User.create({
         login: 'kacperkaz',
@@ -482,26 +294,13 @@ describe('Users Controller', function () {
         role: 'user'
       });
 
-      const req = {
-        body: {
-          login: 'kacperkaz',
-          password: 'dfg'
-        }
+      const body = {
+        login: 'kacperkaz',
+        password: 'dfg'
       };
 
-      await expect(UsersController.login(req)).to.be.rejectedWith(
-        BadRequestError
-      );
-    });
-
-    it('should throw UnprocessableEntityError when credentials are not present', async function () {
-      const req = {
-        body: {}
-      };
-
-      await expect(UsersController.login(req)).to.be.rejectedWith(
-        UnprocessableEntityError
-      );
+      await expect(UsersService.login(body.login, body.password)).to.be
+        .rejected;
     });
   });
   describe('Refresh access token', function () {
@@ -541,19 +340,24 @@ describe('Users Controller', function () {
       user.refresh_token = refreshToken;
       await user.save();
 
-      const req = {
-        body: {
-          token: refreshToken
-        }
-      };
-
-      let response = await UsersController.refreshToken(req);
+      let response = await UsersService.refreshToken(refreshToken);
 
       expect(response).to.have.property('access_token');
       expect(response).to.have.property('refresh_token');
     });
+    it('should throw if token is null', async function () {
+      //create user
+      let user = await User.create({
+        login: 'kacperkaz',
+        email: 'kacperkaz@example.com',
+        password:
+          '$2b$10$ecbupqJoYK4gFeZVcGQfpOYONp5GUUtqheW9AkA6/DHPgGtIGvc6K',
+        role: 'user'
+      });
 
-    it('should throw UnprocessableEntityError when refresh token is malformed', async function () {
+      await expect(UsersService.refreshToken(null)).to.be.rejected;
+    });
+    it('should throw if refresh token is malformed', async function () {
       //create user
       let user = await User.create({
         login: 'kacperkaz',
@@ -580,18 +384,54 @@ describe('Users Controller', function () {
       user.refresh_token = refreshToken;
       await user.save();
 
-      const req = {
-        body: {
-          token: 'asd'
-        }
-      };
+      const token = 'asd';
 
-      await expect(UsersController.refreshToken(req)).to.be.rejectedWith(
-        UnprocessableEntityError
-      );
+      await expect(UsersService.refreshToken(token)).to.be.rejected;
     });
+    it('should throw if refresh token is invalid', async function () {
+      //create user
+      let user = await User.create({
+        login: 'kacperkaz',
+        email: 'kacperkaz@example.com',
+        password:
+          '$2b$10$ecbupqJoYK4gFeZVcGQfpOYONp5GUUtqheW9AkA6/DHPgGtIGvc6K',
+        role: 'user'
+      });
+      //create refresh token
+      let refreshTokenTimestamp =
+        Math.floor(Date.now() / 1000) + 365 * 24 * 60 * 60; // expires in 1 year
+      let refreshToken = jwt.sign(
+        {
+          login: user.login,
+          email: user.email,
+          id: user._id,
+          role: user.role,
+          exp: refreshTokenTimestamp
+        },
+        process.env.JWT_SECRET
+      );
 
-    it('should throw BadRequestError if user is not logged in', async function () {
+      //add refresh token do db
+      user.refresh_token = refreshToken;
+      await user.save();
+
+      //generate wrong refresh token
+      refreshTokenTimestamp =
+        Math.floor(Date.now() / 1000) + 365 * 24 * 60 * 60; // expires in 1 year
+      let wrongToken = jwt.sign(
+        {
+          login: 'asd',
+          email: 'asd@example.com',
+          id: user._id,
+          role: user.role,
+          exp: refreshTokenTimestamp
+        },
+        process.env.JWT_SECRET
+      );
+
+      await expect(UsersService.refreshToken(wrongToken)).to.be.rejected;
+    });
+    it('should throw if user is not logged in', async function () {
       //create user
       let user = await User.create({
         login: 'kacperkaz',
@@ -614,15 +454,7 @@ describe('Users Controller', function () {
         process.env.JWT_SECRET
       );
 
-      const req = {
-        body: {
-          token: refreshToken
-        }
-      };
-
-      await expect(UsersController.refreshToken(req)).to.be.rejectedWith(
-        BadRequestError
-      );
+      await expect(UsersService.refreshToken(refreshToken)).to.be.rejected;
     });
   });
   describe('Logout', function () {
@@ -662,19 +494,13 @@ describe('Users Controller', function () {
       user.refresh_token = refreshToken;
       await user.save();
 
-      const req = {
-        body: {
-          token: refreshToken
-        }
-      };
+      await expect(UsersService.logout(refreshToken)).to.be.fulfilled;
 
-      let response = await UsersController.logout(req);
       let dbUser = await (await User.findById(user._id)).toJSON();
 
       expect(dbUser).to.not.have.property('refresh_token');
     });
-
-    it('should throw BadRequestError if token not present in body', async function () {
+    it('should throw if token is different from the one in a database', async function () {
       //create user
       let user = await User.create({
         login: 'kacperkaz',
@@ -684,7 +510,7 @@ describe('Users Controller', function () {
         role: 'user'
       });
       //create refresh token
-      const refreshTokenTimestamp =
+      let refreshTokenTimestamp =
         Math.floor(Date.now() / 1000) + 365 * 24 * 60 * 60; // expires in 1 year
       let refreshToken = jwt.sign(
         {
@@ -701,13 +527,52 @@ describe('Users Controller', function () {
       user.refresh_token = refreshToken;
       await user.save();
 
-      const req = {
-        body: {}
-      };
-
-      await expect(UsersController.logout(req)).to.be.rejectedWith(
-        BadRequestError
+      //generate wrong refresh token
+      refreshTokenTimestamp =
+        Math.floor(Date.now() / 1000) + 365 * 24 * 60 * 60; // expires in 1 year
+      let wrongToken = jwt.sign(
+        {
+          login: 'asd',
+          email: 'asd@example.com',
+          id: user._id,
+          role: user.role,
+          exp: refreshTokenTimestamp
+        },
+        process.env.JWT_SECRET
       );
+
+      await expect(UsersService.logout(wrongToken)).to.be.rejected;
+    });
+    it('should throw if token is malformed', async function () {
+      //create user
+      let user = await User.create({
+        login: 'kacperkaz',
+        email: 'kacperkaz@example.com',
+        password:
+          '$2b$10$ecbupqJoYK4gFeZVcGQfpOYONp5GUUtqheW9AkA6/DHPgGtIGvc6K',
+        role: 'user'
+      });
+      //create refresh token
+      let refreshTokenTimestamp =
+        Math.floor(Date.now() / 1000) + 365 * 24 * 60 * 60; // expires in 1 year
+      let refreshToken = jwt.sign(
+        {
+          login: user.login,
+          email: user.email,
+          id: user._id,
+          role: user.role,
+          exp: refreshTokenTimestamp
+        },
+        process.env.JWT_SECRET
+      );
+
+      //add refresh token do db
+      user.refresh_token = refreshToken;
+      await user.save();
+
+      let token = 'asd';
+
+      await expect(UsersService.logout(token)).to.be.rejected;
     });
   });
 });
