e878bb1cb1e7fe38387bc34dc85143f521c3df6f
Kacper Kaźmierkiewicz <38405516+KaZiUpl@users.noreply.github.com>
2020-11-12 12:40:11 +0100

    added error messages

--- src/middleware/checkAuth.js
+++ src/middleware/checkAuth.js
@@ -9,23 +9,23 @@ module.exports = async function (req, res, next) {
     // get access token from authorization header
     const authHeader = req.get('Authorization');
     if (!authHeader) {
-      throw new UnauthorizedError();
+      throw new UnauthorizedError('Missing authorization header');
     }
     const token = authHeader.split(' ')[1];
     if (!token) {
-      throw new UnauthorizedError();
+      throw new UnauthorizedError('Wrong authorization header');
     }
     let decoded;
     try {
       decoded = jwt.verify(token, dotenv.jwtSecret);
     } catch (error) {
-      throw new UnauthorizedError();
+      throw new UnauthorizedError('Invalid access token');
     }
     req.userData = decoded;
 
     let user = await User.findById(req.userData.id);
     if (!user.refresh_token) {
-      throw new UnauthorizedError();
+      throw new UnauthorizedError('User not logged in');
     }
 
     next();
