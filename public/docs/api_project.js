cac1505526aafa83d8a4542fec4c342597f798cb
Kacper Kaźmierkiewicz <38405516+KaZiUpl@users.noreply.github.com>
2020-12-22 12:03:24 +0100

    updated docs

--- public/docs/api_project.js
+++ public/docs/api_project.js
@@ -9,7 +9,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2020-12-06T10:42:44.540Z",
+    "time": "2020-12-22T11:03:12.846Z",
     "url": "http://apidocjs.com",
     "version": "0.24.0"
   }
