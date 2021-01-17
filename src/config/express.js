c40eee90b829a1ec44dffbf23630d75ad2fa2d7d
Kacper Kaźmierkiewicz <38405516+KaZiUpl@users.noreply.github.com>
2021-01-17 10:41:21 +0100

    Update express.js

--- src/config/express.js
+++ src/config/express.js
@@ -27,7 +27,7 @@ app.use(helmet.hidePoweredBy());
 if (process.env.NODE_ENV && process.env.NODE_ENV.toLowerCase() !== 'test') {
   app.use(morgan('dev'));
 }
-console.log(process.cwd() + '/public');
+
 //set static documentation files
 app.use(express.static(path.join(process.cwd() + '/public')));
 app.use('/docs', express.static('public/docs'));
